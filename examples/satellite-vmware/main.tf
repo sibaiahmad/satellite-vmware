ca98de9cf8bd00ee96c2957480c415cee862805c
sibaiahmad <99640477+sibaiahmad@users.noreply.github.com>
2023-12-19 12:45:13 +0300

    Update main.tf
    Added OCP Cluster creation

--- examples/satellite-vmware/main.tf
+++ examples/satellite-vmware/main.tf
@@ -276,3 +276,47 @@ module "satellite-host" {
   location_zones = var.location_zones
   host_labels    = var.host_labels
 }
+
+###################################################################
+# Create satellite ROKS cluster
+###################################################################
+module "satellite-cluster" {
+  //Uncomment following line to point the source to registry level module
+  //source = "git::git@github.com:terraform-ibm-modules/terraform-ibm-satellite.git//modules/cluster"
+
+  source                     = "./modules/cluster"
+  create_cluster             = var.create_cluster
+  cluster                    = var.cluster
+  location                   = module.satellite-location.location_id
+  kube_version               = var.kube_version
+  zones                      = local.location_zones
+  resource_group             = var.resource_group
+  worker_count               = var.worker_count
+  host_labels                = var.cluster_host_labels
+  default_worker_pool_labels = var.default_worker_pool_labels
+  create_timeout             = var.create_timeout
+  update_timeout             = var.update_timeout
+  delete_timeout             = var.delete_timeout
+  depends_on                 = [module.satellite-host]
+}
+
+###################################################################
+# Create worker pool on existing ROKS cluster
+###################################################################
+module "satellite-cluster-worker-pool" {
+  //Uncomment following line to point the source to registry level module
+  //source = "terraform-ibm-modules/satellite/ibm//modules/configure-cluster-worker-pool"
+
+  source                     = "./modules/configure-cluster-worker-pool"
+  create_cluster_worker_pool = var.create_cluster_worker_pool
+  worker_pool_name           = var.worker_pool_name
+  cluster                    = var.cluster
+  zones                      = local.location_zones
+  resource_group             = var.resource_group
+  kube_version               = var.kube_version
+  worker_count               = var.worker_count
+  host_labels                = var.worker_pool_host_labels
+  create_timeout             = var.create_timeout
+  delete_timeout             = var.delete_timeout
+  depends_on                 = [module.satellite-cluster]
+}
