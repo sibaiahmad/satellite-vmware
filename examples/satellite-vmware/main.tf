e7786989db7c709975f3f8ff82b8afb4c59867f6
sibaiahmad <99640477+sibaiahmad@users.noreply.github.com>
2023-12-22 21:54:49 +0300

    Update main.tf

--- examples/satellite-vmware/main.tf
+++ examples/satellite-vmware/main.tf
@@ -277,4 +277,50 @@ module "satellite-host" {
   host_labels    = var.host_labels
 }
 
+################### New Cluster Creation #################
+
+###################################################################
+# Create satellite ROKS cluster
+###################################################################
+module "satellite-cluster" {
+  source = "terraform-ibm-modules/satellite/ibm//modules/cluster"
+
+  create_cluster             = var.create_cluster
+  cluster                    = var.cluster
+  zones                      = var.location_zones
+  location                   = var.location_id
+  resource_group             = var.resource_group
+  kube_version               = var.kube_version
+  worker_count               = var.worker_count
+  host_labels                = var.host_labels
+  tags                       = var.tags
+  default_worker_pool_labels = var.default_worker_pool_labels
+  create_timeout             = var.create_timeout
+  update_timeout             = var.update_timeout
+  delete_timeout             = var.delete_timeout
+  operating_system           = var.operating_system
+
+  depends_on                 = [module.satellite-host]
+}
+
+###################################################################
+# Create worker pool on existing ROKS cluster
+###################################################################
+module "satellite-cluster-worker-pool" {
+  source = "terraform-ibm-modules/satellite/ibm//modules/configure-cluster-worker-pool"
+
+  create_cluster_worker_pool = var.create_cluster_worker_pool
+  worker_pool_name           = var.worker_pool_name
+  cluster                    = var.cluster
+  zones                      = var.location_zones
+  resource_group             = var.resource_group
+  kube_version               = var.kube_version
+  worker_count               = var.worker_count
+  host_labels                = var.worker_pool_host_labels
+  create_timeout             = var.create_timeout
+  delete_timeout             = var.delete_timeout
+  operating_system           = var.operating_system
+
+  depends_on                 = [module.satellite-cluster]
+}
 
